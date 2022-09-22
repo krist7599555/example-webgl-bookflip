685d96259520d8b4cb24888cd1559637bc42b2f3
krist7599555 <krist7599555@gmail.com>
2022-09-23 00:41:21 +0800

    feat: color

--- src/test_proxygl.ts
+++ src/test_proxygl.ts
@@ -1,20 +1,25 @@
 import { createProxy } from "./lib/proxygl";
 import { assert, createProgram, createShader } from "./lib/krgl/helper";
+import { WEBGL_TYPE_TABLE } from "./lib/sgl/helper";
 
 const vs = /*glsl*/ `#version 300 es
 #pragma vscode_glsllint_stage: vert
 in vec2 a_position;
+in vec3 a_color;
+out vec3 v_color;
 void main() {
   gl_PointSize = 50.0;
+  v_color = a_color;
   gl_Position = vec4(a_position, 0.0, 1.0);
 }
 ` as const;
 const fs = /*glsl*/ `#version 300 es
 #pragma vscode_glsllint_stage: frag
 precision mediump float;
+in vec3 v_color;
 out vec4 color;
 void main() {
-  color = vec4(1.0, 0.0, 0.0, 1.0);
+  color = vec4(v_color, 1.0);
 }
 ` as const;
 
@@ -36,21 +41,41 @@ export function test_proxygl(canvas: HTMLCanvasElement) {
         size: 1;
         type: "FLOAT_VEC2";
       };
+      a_color: {
+        size: 1;
+        type: "FLOAT_VEC3";
+      };
     };
     uniforms: {};
   }>(gl, program);
   // @ts-ignore
   window["p"] = p;
-  // p.vertext_array.attributes.a_position.enabled = false;
-  // gl.vertexAttrib2f(p.vertext_array.attributes.a_position.location, -0.5, -0.3);
+
+  /* -------------------------------- SET DATA -------------------------------- */
 
   p.array_buffer = gl.createBuffer(); // internaly bind
-  p.array_buffer_data = new Float32Array([-0.7, -0.5, 0.6, 0, 0, 0.7]);
+  // prettier-ignore
+  p.array_buffer_data = new Float32Array([
+    -0.7, -0.5,   1, 0, 0,
+    0.6, 0,       0, 1, 0,
+    0, 0.7,       0, 0, 1,
+  ]);
 
-  p.vertext_array.attributes.a_position.enabled = true;
-  p.vertext_array.attributes.a_position.update_vertex_attrib_pointer();
+  const { a_position, a_color } = p.vertext_array.attributes;
+  const sz_float = WEBGL_TYPE_TABLE.FLOAT.size_byte;
 
-  console.log("HELLO");
+  a_position.enabled = true;
+  a_position.offset = sz_float * 0;
+  a_position.stripe = sz_float * 5;
+
+  a_color.enabled = true;
+  a_color.offset = sz_float * 2;
+  a_color.stripe = sz_float * 5;
+
+  p.array_buffer = null;
+
+  /* ---------------------------------- DRAW ---------------------------------- */
+
   gl.clearColor(1, 0.7, 0.8, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
